fd5289b93670093365225248a3b28315c01cd3d9
danielleeehahn <dmhahn@ncsu.edu>
2023-07-28 15:38:57 +0000

    final updates

--- can-summer-2023/notebooks/Dani/api/NOCOMMIT_dani_mapbox_token.py
+++ can-summer-2023/notebooks/Dani/api/NOCOMMIT_dani_mapbox_token.py
@@ -1,7 +1,7 @@
 # Store the mapbox isochrone token
 
 def my_token():
-    token = 'sk.eyJ1IjoiZGFuaWVsbGVlZWhhaG4iLCJhIjoiY2xqdDM2OHBiMHI3ajNmbzg2b25hMjVibyJ9.suuQt6COqNYq0rJnjYgimQ'
+    token = 
     
     return token
 
