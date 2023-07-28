9260acbe0707facf5288bc985c3c7f1297accdc6
Tasnim <Tasneemalqudah7@gmail.com>
2023-07-28 16:31:31 +0000

    Save changes

--- can-summer-2023/notebooks/api/mapbox_token.py
+++
@@ -1,6 +0,0 @@
-# Store the mapbox isochrone token
-
-def my_token():
-    token = 'pk.eyJ1IjoidGFzbmltcXUiLCJhIjoiY2xqcmp3ZmJxMGR5aTNlcndkaWxobWtnMyJ9.CL0UPae46_yFnA3XELq3yA'
-    
-    return token
