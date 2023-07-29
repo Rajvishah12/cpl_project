49c27a20ff53a2a0df790dc4d847de68a59863e5
Rajvishah12 <rajvis2011@gmail.com>
2023-07-29 05:56:35 +0000

    organized notebooks and datasets

---
+++ can-summer-2023/notebooks/Rajvi - Miscellaneous/api things/DO_NOT_COMMIT_rajvi_mapbox_token.py
@@ -0,0 +1,7 @@
+# Store the mapbox isochrone token
+
+def my_token():
+    #token = 'sk.eyJ1IjoicnNoYWgyNzYiLCJhIjoiY2xqc3Iyem01MG9lZjNla2NvOG5xN3o4ayJ9.59-CWx-iDUQwqvcAiar7Gw'
+    
+    return token
+ 
